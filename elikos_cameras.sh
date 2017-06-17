51191d552ebfbedf05c70127b8feb5fb7969c4e2
Elikos default <info@elikos.polymtl.ca>
2017-06-17 18:54:47 -0400

    Enregistrement des cameras.

---
+++ elikos_cameras.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Valeurs par défauts
+START=true
+STOP=false
+
+# Extraction des paramètres
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    start)
+    START=true
+    STOP=false
+    shift
+    ;;
+    stop)
+    START=false
+    STOP=true
+    shift
+    ;;
+    *)
+    ;;
+esac
+shift
+done
+
+# Démarrage des processus.
+if [ "$START" = true ] ; then
+    echo 'Démarrage des processus!'
+    source ~/ros-workspaces/elikos-ws/devel/setup.bash
+    roslaunch elikos_ros elikos_realsense_left.launch > /dev/null &
+    sleep 2
+    roslaunch elikos_ros elikos_realsense_right.launch > /dev/null &
+    sleep 2
+    roslaunch elikos_ros elikos_realsense_front.launch > /dev/null &
+    sleep 2
+    roslaunch elikos_ros elikos_pointgrey_bottom.launch > /dev/null &
+    sleep 2
+# Arrêt des processus.
+elif [ "$STOP" = true ] ; then
+    echo 'Arrêt des processus!'
+    killall roslaunch
+fi
