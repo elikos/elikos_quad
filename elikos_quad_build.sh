16b5cc9a419cb1f6c9a40537adabe129cba457ff
christophebedard <chrisvolkoff@gmail.com>
2017-07-01 12:25:44 -0400

    Add submodules init and dependencies installation to script

--- elikos_quad_build.sh
+++ elikos_quad_build.sh
@@ -1,18 +1,69 @@
 #!/bin/bash
 
-echo 'driver-ws'
-catkin build --workspace driver-ws/
-echo 'Sourcing driver-ws'
-source driver-ws/devel/setup.bash
+# Valeurs par défauts
+SUBMODULES=false
+DEPENDENCIES=false
+BUILD=false
 
-echo 'util-ws'
-catkin build --workspace util-ws/
-echo 'Sourcing util-ws'
-source util-ws/devel/setup.bash
+# Extraction des paramètres
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    build)
+    BUILD=true
+    shift
+    ;;
+    init)
+    if [ "$2" = --dep ] ; then
+        DEPENDENCIES=true
+    elif [ "$2" = --submod ] ; then
+        SUBMODULES=true
+    fi
+    if [ "$3" = --dep ] ; then
+        DEPENDENCIES=true
+    elif [ "$3" = --submod ] ; then
+        SUBMODULES=true
+    fi
+    shift
+    ;;
+    *)
+    ;;
+esac
+shift
+done
 
-echo 'elikos-ws'
-catkin build --workspace elikos-ws/
-echo 'Sourcing elikos-ws'
-source elikos-ws/devel/setup.bash
+# Dependencies
+if [ "$DEPENDENCIES" = true ] ; then
+    echo 'Installation des dépendances (indigo)!'
+    sudo apt-get install -y ros-indigo-mavros ros-indigo-mavros-extras ros-indigo-pointgrey-camera-driver ros-indigo-moveit
+fi
+
+# Submodules init
+if [ "$SUBMODULES" = true ] ; then
+    echo 'Initialisation des submodules!'
+    #git submodule init
+	#git submodule update --recursive --remote
+fi
+
+# Build
+if [ "$BUILD" = true ] ; then
+    echo 'Building!'
+
+    echo 'driver-ws'
+    catkin build --workspace driver-ws/
+    echo 'Sourcing driver-ws'
+    source driver-ws/devel/setup.bash
+
+    echo 'util-ws'
+    catkin build --workspace util-ws/
+    echo 'Sourcing util-ws'
+    source util-ws/devel/setup.bash
+
+    echo 'elikos-ws'
+    catkin build --workspace elikos-ws/
+    echo 'Sourcing elikos-ws'
+    source elikos-ws/devel/setup.bash
+fi
 
 echo 'All done!'
